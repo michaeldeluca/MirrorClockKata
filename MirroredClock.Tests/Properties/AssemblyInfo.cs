66b9edf872ac0db2062efae0ebc56ce45040ad1f
Michael DeLuca <michael.deluca@zeninternet.co.uk>
2021-08-02 09:53:07 +0100

    Add project files.

---
+++ MirroredClock.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MirroredClock.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MirroredClock.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bed0c03c-180c-44fc-b8fe-c2526abaf6e0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MirroredClock.Tests/TimeProcessorTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MirroredClock.Tests
+{
+    [TestClass]
+    public class MirroredClockTests
+    {
+        [DataTestMethod]
+        [DataRow("12:22", "11:38")]
+        [DataRow("01:50", "10:10")]
+        [DataRow("11:58", "12:02")]
+        [DataRow("12:01", "11:59")]
+        [DataRow("12:02", "11:58")]
+        [DataRow("10:36", "01:24")]
+        [DataRow("12:00", "12:00")]
+        [DataRow("11:00", "01:00")]
+        public void Mirrored_time_is_correct(string time, string expected)
+        {
+            var actual = new TimeConverter().Mirror(time);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class TimeConverter
+    {
+        public string Mirror(string time)
+        {
+            var timeAsDateTime = DateTime.Parse(time);
+
+            return new DateTime(2021, 01, 01)
+                      .AddHours(11 - timeAsDateTime.Hour)
+                      .AddMinutes(60 - timeAsDateTime.Minute)
+                      .ToString("hh:mm");
+        }
+    }
+}
